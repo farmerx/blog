390f1c110d63b38fb1df51080dfebd6f1188b0ae
Farmerx <90bibin@gmail.com>
2018-02-27 17:57:51 +0800

    Create curl_proxy_1.go

---
+++ curl_proxy_1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+type handle struct {
+	host string
+	port string
+}
+
+func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	remote, err := url.Parse("http://" + this.host + ":" + this.port)
+	if err != nil {
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.ServeHTTP(w, r)
+}
+
+func startServer() {
+	//被代理的服务器host和port
+	h := &handle{host: "10.95.154.63", port: "8080"}
+	err := http.ListenAndServe(":9090", h)
+	if err != nil {
+		log.Fatalln("ListenAndServe: ", err)
+	}
+}
+
+func main() {
+	startServer()
+}
